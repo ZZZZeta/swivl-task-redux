e5c86b5af17f07253e7335354a9a221c503b319a
Yan Postnov <y.postnov@helsi.me>
2021-01-25 10:05:51 +0200

    fix gh-pages deployment issues

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import { UserList } from '../pages/users/UserList';
 import { User } from '../pages/users/User';
 
 const Router = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <Switch>
       <Route exact path="/" component={UserList} />
       <Route exact path="/user/:username" component={User} />
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { createLogger } from 'redux-logger';
 import rootReducer from '../redux/rootReducer';
 
 // middlewares
 import { apiMiddleware } from './middlewares/apiMiddleware';
 
-const middlewares = [
+const middleware = applyMiddleware(
   apiMiddleware,
-  process.env.NODE_ENV === 'development' && createLogger({ collapsed: true }),
-];
-
-// @ts-ignore
-const middleware = applyMiddleware(...middlewares);
+);
 
 const initialState = {};
 
